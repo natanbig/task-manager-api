bc20634b29746c118a0b74980beb5c038b0356f1
Natan Radostin <natanbig@attunity.com>
2020-05-27 19:32:04 +0300

    Fixed TOKEN_SECRET

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 module.exports = auth = async (ctx, next) => {
     try {
         const token = ctx.request.get('Authorization').replace('Bearer ', '')
-        const decoded = await jwt.verify(token, 'thisismysecretkey')
+        const decoded = await jwt.verify(token, process.env.TOKEN_SECRET)
         const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
         if (!user) {
             throw new Error()
